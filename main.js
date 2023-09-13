4d421d325b3eb85e132856c7dce43b060dfa1884
jonathanMed <jhony270315@gmail.com>
2023-09-12 18:31:09 -0600

    Fusión de menus, carrito de compras y opciones. Lógica para aparecer los menus dependiendo que menus esten abiertos.

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-let menuEmail = document.querySelector('.navbar-email');
-let desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const carritoIcon = document.querySelector('.navbar-shopping-cart');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarrito = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+carritoIcon.addEventListener('click', toggleCarritoMenu);
 
 function toggleDesktopMenu() {
+    const ismenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    
+    if(!ismenuCarritoClosed) {
+        menuCarrito.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
+}
 
-    /* desktopMenu.classList.add('inactive'); */
+function toggleMobileMenu() {
+    const ismenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    
+    if(!ismenuCarritoClosed) {
+        menuCarrito.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoMenu() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    menuCarrito.classList.toggle('inactive');
 }
