ab2f7e078c459270045704d0b076afde9d75cd06
jonathanMed <jhony270315@gmail.com>
2023-09-14 18:27:05 -0600

    Creación de la función addProduct para añadir productos al HTML.

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const carritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarrito = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+/* const mainContainer = document.querySelector('.main-container'); */
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -49,4 +51,74 @@ function toggleCarritoMenu() {
         price: 250,
         img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     });
+    productList.push({
+        name: 'pantalla',
+        price: 300,
+        img: 'https://images.pexels.com/photos/7546602/pexels-photo-7546602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+    productList.push({
+        name: 'Laptop',
+        price: 630,
+        img: 'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+     productList.push({
+        name: 'Iphone',
+        price: 900,
+        img: 'https://images.pexels.com/photos/5083491/pexels-photo-5083491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+    productList.push({
+        name: 'Xbox Controller',
+        price: 250,
+        img: 'https://images.pexels.com/photos/16776031/pexels-photo-16776031/free-photo-of-internet-conexion-tecnologia-negocio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+    productList.push({
+        name: 'Xbox Play Station',
+        price: 300,
+        img: 'https://images.pexels.com/photos/14824332/pexels-photo-14824332.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    });
+    productList.push({
+        name: 'Jordan retro 1',
+        price: 200,
+        img: 'https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&w=600'
+    });
 
+    /* const parrafo = document.createElement('p'); */
+
+
+function addProduct(arr) {
+    for (product of arr) {
+
+        const figureImg = document.createElement('img');
+        figureImg.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        const figure = document.createElement('figure');
+        figure.append(figureImg);
+
+        const parrafoName = document.createElement('p');
+        parrafoName.innerText = product.name;
+
+        const parrafoPrice = document.createElement('p');
+        parrafoPrice.innerText = '$' + product.price;
+
+        const divInfo = document.createElement('div');
+        divInfo.append(parrafoPrice, parrafoName);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+        productInfo.append(divInfo, figure);
+
+        const imgProduct = document.createElement('img');
+        imgProduct.setAttribute('src', product.img);
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        productCard.append(imgProduct, productInfo);
+
+       /*  const cardsContainer = document.createElement('div');
+        cardsContainer.classList.add('.cards-container'); */
+        cardsContainer.append(productCard);
+
+    }
+}
+
+addProduct(productList);
